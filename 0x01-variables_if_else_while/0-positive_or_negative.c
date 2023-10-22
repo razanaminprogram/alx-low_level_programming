72b770786eee5f920fee1991fcfebf8cd0c0cf1a
razanaminprogram <razanamin5@gmail.com>
2023-10-22 12:25:44 -0700

    p or n

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - A program for positive and negative
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n); }
+if (n < 0)
+{
+printf("%d is negative\n", n); }
+if (n == 0)
+{
+printf("%d is zero\n", n); }
+	return (0);
+}
