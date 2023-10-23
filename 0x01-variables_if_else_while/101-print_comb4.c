75f4d00bc7be792ad065d8e71a38fb56059e1a23
razanaminprogram <razanamin5@gmail.com>
2023-10-23 10:01:39 -0700

    four

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - A program that prints combinations of three nums
+ * Return: 0 (Success)
+ */
+int main() {
+int i, j, k;
+for (i = 0; i < 8; i++)
+{
+for (j = i + 1; j < 9; j++)
+{
+for (k = j + 1; k < 10; k++)
+{
+putchar('0' + i);
+putchar('0' + j);
+putchar('0' + k);
+if (i != 7 || j != 8 || k != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return 0;
+}
