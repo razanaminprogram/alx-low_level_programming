626b4549c7dd6b7ef5ea0ce10057ff5e04ce87b8
razanaminprogram <razanamin5@gmail.com>
2024-03-26 16:34:31 -0700

    time table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+#include "main.h"
+
+/**
+ * print_times_table - prints the time table of the input
+ * @n: the number which its time table will be printed
+ * Return - 0 Successful
+ */
+
+void print_times_table(int n)
+{
+	int x, i, m ;
+	if (n >= 0 && n <= 15)
+	{
+		for(i = 0; i <= n; i++)
+		{
+     for(m = 0; m <= n; m++)
+     {
+	     x= n * m;
+	     printf("%d,", x);
+     }
+     printf("\n");
+		}
+	}
+	}
+
