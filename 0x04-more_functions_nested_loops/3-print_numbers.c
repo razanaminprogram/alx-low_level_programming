98ed193669934670476c96dbcfeb970d5cf9fb93
razanaminprogram <razanamin5@gmail.com>
2024-03-28 22:56:02 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+for (i = 48; i <= 57; i++)
+	{
+_putchar(i);
+	}
+	_putchar('\n');
+}
