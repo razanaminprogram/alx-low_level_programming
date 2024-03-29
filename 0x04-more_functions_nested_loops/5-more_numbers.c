0b37a568d54727df72d0c17355abbccf99931655
razanaminprogram <razanamin5@gmail.com>
2024-03-29 00:08:34 -0700

    14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,17 +13,10 @@ for (x = 0; x < 10; x++)
 {
 for (y = 0; y <= 14; y++)
 {
-if (y < 10)
-{
-_putchar('0' + y);
-}
-else
-{
-_putchar('1');
+_putchar((y < 10) ? '0' + y : ((y == 10) ? '1' : '0'));
+if (y != 10)
 _putchar((y % 10) + '0');
-}
 }
 _putchar('\n');
 }
 }
-
