21861c94bf92c8b1394f1884b95012ccd754536e
razanaminprogram <razanamin5@gmail.com>
2024-03-29 01:27:39 -0700

    _
    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 6-main.c 6-print_line.c -o 6-lines
    
    clear:

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints straight line
+ * @n: is the number of times the char _ be printed
+ * Return: Success
+ */
+
+void print_line(int n)
+{
+	int i;
+
+for (i = 1; i <= n; i++)
+{
+_putchar('_');
+}
+		_putchar('\n');
+}
