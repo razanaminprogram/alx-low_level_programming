b8ba92f54fafb5485d818b0303265b7b49d29634
razanaminprogram <razanamin5@gmail.com>
2024-03-28 22:27:57 -0700

    multiiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multipies two integers
+ * @a: the first number
+ * @b: the second number
+ * Return: Success
+ */
+
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
