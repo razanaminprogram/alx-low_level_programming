8b89eaf2a7a6d08d19aa462e37cbf51fa4cdc719
razanaminprogram <razanamin5@gmail.com>
2024-03-29 13:21:17 -0700

    #

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,7 +2,7 @@
 
 /**
  * print_square - prints  a square
- * @size: the size of the square 
+ * @size: the size of the square
  * Return: Success
  */
 
@@ -10,13 +10,19 @@ void print_square(int size)
 {
 	int i, x;
 
-	for (i = 1; i <= size; i++)
+	if (size <= 0)
 	{
-		for (x = 1; x <= size; x++)
+		_putchar('\n');
+	}
+	else
+	{
+	for (i = 0; i < size; i++)
+	{
+		for (x = 0; x < size; x++)
 		{
 			_putchar(35);
 		}
-		_putchar(32);
 		_putchar('\n');
 	}
+}
 }
