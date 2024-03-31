6e7cdd85aca31def4a7e17e79e7647d3bd8a4f06
razanaminprogram <razanamin5@gmail.com>
2024-03-30 20:19:03 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,13 +11,13 @@ void puts2(char *str)
 	int i;
 	int j = 0;
 
-	while (str[j] != '\0')
-	{
-		j++;
-	}
-	for (i = 0; i < j; i+= 2)
-	{
-			_putchar(str[i]);
-	}
-	_putchar('\n');
+while (str[j] != '\0')
+{
+j++;
+}
+for (i = 0; i < j; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
