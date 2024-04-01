c163754c235da7666ab6c1cd4d2d8c51b54abb43
razanaminprogram <razanamin5@gmail.com>
2024-03-31 18:43:07 -0700

    2strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @src: the string will be appended 
+ * @dest: the string that will be appended to
+ * Return: the pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len = 0, i;
+
+	while (dest[len])
+	{
+		len++;
+	}
+for (i = 0; src[i] != 0; i++)
+{
+	dest[len] = src[i];
+}
+dest[len] = '\0';
+return (dest);
+}
