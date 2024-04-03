fe55aeb085500035f44d49a35ec3b789c49b803b
razanaminprogram <razanamin5@gmail.com>
2024-04-02 22:25:31 -0700

    print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the integer
+ * Return: void 
+ */
+
+void print_number(int n)
+{
+	n = n + '0';
+	_putchar(n);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,3 +7,4 @@ char *string_toupper(char *);
 char *cap_string(char *);
 int _putchar(char c);
 char *leet(char *);
+void print_number(int n);
